b19f4da575f01a06a070bd2c3cc65a635b654f2d
Aniruddha Maru <aniruddhamaru@gmail.com>
2018-04-23 15:46:26 -0700

    Fix outputs.tf

--- modules/queue/outputs.tf
+++ modules/queue/outputs.tf
@@ -15,14 +15,6 @@ output "dlq_name" {
 }
 
 output "high_message_alert_id" {
-  value = "${module.alerts.high_message_alert_id}"
-}
-
-output "dlq_alert_id" {
-  value = "${module.alerts.dlq_alert_id}"
-}
-
-output "high_message_alert_id" {
   value = "${aws_cloudwatch_metric_alarm.high_message_alert.id}"
 }
 
