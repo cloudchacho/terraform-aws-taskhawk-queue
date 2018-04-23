a3f1dd6076a638ff8994da393adb5b80203a39b8
Aniruddha Maru <aniruddhamaru@gmail.com>
2018-04-23 15:56:01 -0700

    Remove conditional things from output

--- modules/queue/outputs.tf
+++ modules/queue/outputs.tf
@@ -13,11 +13,3 @@ output "dlq_arn" {
 output "dlq_name" {
   value = "${aws_sqs_queue.dlq.name}"
 }
-
-output "high_message_alert_id" {
-  value = "${aws_cloudwatch_metric_alarm.high_message_alert.id}"
-}
-
-output "dlq_alert_id" {
-  value = "${aws_cloudwatch_metric_alarm.dlq_alert.id}"
-}
